29da2fc2a43866fa3b521dbfd8a9a8621be631f2
PierreEmad-PEPO <piereemorgan@gmail.com>
2024-03-03 01:28:51 +0200

    delete cell

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,39 +6,4 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
-    Vector3 startPos;
-    Vector3 endPos;
-    Cell tagetCell;
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    public void startAttack(Vector3 stapos)
-    {
-        this.startPos = stapos;
-        GetRandomCell();
-    }
-
-    void GetRandomCell()
-    {
-        List<GameObject> cells = new List<GameObject>();
-        foreach (var list in GridManager.Grid)
-        {
-            foreach (var cell in list)
-            {
-                if (cell.GetComponent<Cell>().IsPlanting)
-                    cells.Add(cell);
-            }
-        }
-
-        GameObject tagetObject = cells[Random.Range(0, cells.Count)];
-        endPos = tagetObject.transform.position;
-        tagetCell = tagetObject.GetComponent<Cell>();
-
-    }
-
-
 }
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -5,25 +5,5 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0)) 
-        {
-            Tuple<int, int> pos = InputManager.MouseToGrid();
-            Debug.Log(pos.Item1 + "  " + pos.Item2);
-            if (pos.Item1 <= 3 && pos.Item2 <= 3)
-            {
-                GridManager.Grid[pos.Item1][pos.Item2].GetComponent<Cell>().Click(PlantEnum.gazr);
-                //GridManager.Grid[pos.Item1][pos.Item2].GetComponent<Cell>().StartCorruption();
-            }
-        }
-    }
+    
 }
